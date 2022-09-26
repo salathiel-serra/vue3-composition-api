3a7b1baa15629d604d12352e418619ce28829e50
Thiel Serra <thiel.serra@gmail.com>
2022-09-26 18:41:48 -0300

    Estrutura do projeto

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HelloWorld msg="Vue 3 | Composição de API"/>
   </div>
 </template>
 
---
+++ src/views/ToDos/ToDos.vue
@@ -0,0 +1,3 @@
+<template>
+    <h1> Lista de tarefas </h1>
+</template>
