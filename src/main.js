cd60df7d94711cb6e3eaea2913815a13a5419b99
Thiel Serra <thiel.serra@gmail.com>
2022-09-27 11:40:18 -0300

    Adicionando axios ao projeto, implementando busca de dados em API e exibindo dados

--- src/main.js
+++ src/main.js
@@ -1,7 +1,12 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
+import { createApp } from 'vue';
+import App from './App.vue';
+import router from './router';
+
+import axios from "axios";
+import { URL_API } from "./configs/api";
+
+axios.defaults.baseURL = URL_API;
 
 createApp(App)
     .use(router)
-    .mount('#app')
+    .mount('#app');
--- src/views/ToDos/ToDos.vue
+++ src/views/ToDos/ToDos.vue
@@ -1,3 +1,35 @@
 <template>
     <h1> Lista de tarefas </h1>
+    <hr>
+    <ul>
+        <li v-for="todo in todoList" :key="todo.identify">{{todo.title}}</li>
+    </ul>
 </template>
+
+<script>
+    import { onMounted, ref } from "vue";
+    import TodoService from '@/services/todos.service';
+
+    export default {
+        name: 'ToDos',
+        setup() {
+            const todoList = ref([]);
+
+            onMounted(() => {
+                TodoService.getAll()
+                    .then(response => {
+                        // console.log(response);
+                        todoList.value = response.data.data;
+                    })
+                    .catch(error => {
+                        console.log(error);
+                    })
+            })
+
+            return {
+                todoList
+            }
+        }
+
+    }
+</script>
